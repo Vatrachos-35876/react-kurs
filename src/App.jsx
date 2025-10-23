13eb95bde7f7f72d13eff33beb1d64b39f8e80f2
Vatrachos-35876 <jakubbrodka08@gmail.com>
2025-10-23 13:32:54 +0200

    Conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,47 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => {
-  return (
-    <div>{counter}</div>
-  )
-}
-
-const Button = ({ onClick , text }) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
-
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const add = () => setCounter(counter + 1)
-  const rem = () => setCounter(counter - 1)
-  const zero = () => setCounter(0)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={add} text="Add" />
-      <Button onClick={zero} text="Zero" />
-      <Button onClick={rem} text="Remove" />
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
